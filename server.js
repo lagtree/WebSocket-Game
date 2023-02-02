05bf60979c79490b6b4da3d516b8b624a78e5ad7
Zachary V <43046943+lagtree@users.noreply.github.com>
2023-02-01 21:24:01 -0500

    Changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+
 const http = require("http");
 const { PassThrough } = require("stream");
 const app = require("express")();
---
+++ test2/test2.js
@@ -0,0 +1,105 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import { WebSocketServer } from 'ws';
+
+import fetch from "node-fetch";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const clients = new Set();
+
+var potential = [];
+
+var answers = null;
+var pointsOut = null;
+
+var response = await fetch('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt');
+var openedText = await response.text(); // <-- changed
+var words = openedText.split(/\r\n|\n/);
+
+wss.on('connection', (ws) => {
+  clients.add(ws);
+
+  ws.on('message', (message) => {
+   // process the data received from the client
+    firstWord();
+    let processedData = processData(message);
+    console.log("debug: " + processedData)
+
+ 
+    // send the processed data back to all clients
+    for (const client of clients) {
+      client.send(processedData);
+    }
+  });
+
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+});
+
+//read from file a list of words, save it in a list and and shuffle it (twice for good measure)
+
+for(var j = 0; j < words.length; j++) {
+    if (words[j].length === 6) {
+        potential.push(words[j]);
+    }
+}
+
+
+function allAnagrams(word) {
+    
+    var anaWord = word;
+    
+    var all = [];
+    for(var i = 0; i < words.length; i++) {
+
+        var currentWord = words[i];
+        anaWord = word;
+        
+        if(currentWord.length <= anaWord.length) {
+            var isAnagram = true;
+            for(var k = 0; k < currentWord.length; k++) {
+                var current = currentWord.substring(k, k+1);
+                var indOf = anaWord.indexOf(current);
+                
+                
+                if(indOf > -1) {
+                    anaWord = anaWord.split("");
+                    anaWord.splice(indOf, 1);
+                    anaWord = anaWord.join('');
+                } else {
+                    isAnagram = false;
+                }
+            }
+                
+            if (isAnagram) {
+                all.push(currentWord);
+            }
+        }
+    }
+    return all;
+}
+
+function firstWord() {
+    let rand = Math.floor(Math.random()*potential.length);
+    let w = potential[rand];
+    answers = allAnagrams(w);
+    console.log(potential.length);
+    console.log('w: ' + w);
+    console.log('answers: ' + answers);
+}
+
+function processData(word) {
+    if(answers != null){
+        for(let k = 0; k < answers.length; k++) {
+            if (word == answers[k]) {
+                let pointsOut = word.length * 100;
+            }else {
+                let pointsOut = 0;
+            }
+        }
+    }
+    return pointsOut;
+}
